5663dbbaacd95abf5fc4a7be92390805916159cf
innocenat <innocenat@server.fake>
2014-02-10 21:32:24 +0700

    Fix some testing change that I forget to revert

--- AvsCheck/testlib/output.cpp
+++ AvsCheck/testlib/output.cpp
@@ -3,7 +3,7 @@
 #include "output.h"
 
 bool AVSC_DISPATCH_FUNC(avsc_compare_output_planar)(const BYTE* p1, const BYTE* p2, int p1_pitch, int p2_pitch, int width, int height, double* avg_diff, int* max_diff) {
-    AVSC_DISPATCH(AVSC_AVX512, avsc_compare_output_planar, avsc_compare_output_planar_sse2)(p1, p2, p1_pitch, p2_pitch, width, height, avg_diff, max_diff);
+    AVSC_DISPATCH(AVSC_SSE2, avsc_compare_output_planar, avsc_compare_output_planar_sse2)(p1, p2, p1_pitch, p2_pitch, width, height, avg_diff, max_diff);
     return false;
 }
 
